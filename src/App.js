645cc01ae7662afbcc6e13c39d451ac837475b58
Huynh Nhat Vuong <huynhnhatvuong01012002@gmail.com>
2024-08-21 23:19:14 +0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-multi-carousel/lib/styles.css";
-import ReactElasticCarousel from "react-elastic-carousel";
 
 const responsive = {
   desktop: {
